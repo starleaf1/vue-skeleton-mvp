2d2f4c0e91175aae34d6624db85f630538710d0d
Anugerah Christian Rompis <starleaf1@gmail.com>
2022-11-21 21:35:40 +0800

    Migrated references to Vuex stores into Pinia.

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Meta from 'vue-meta'
 import routes from '@/router/routes'
-import { store } from '@/store'
+// import { store } from '@/store'
+import { useAuthStore } from '@/store/auth'
 import { checkIfTokenNeedsRefresh } from '@/utils/utils.js'
 import { checkForUpdates } from '@/utils/updates.js'
-import * as types from '@/store/mutation-types'
+// import * as types from '@/store/mutation-types'
+import { useSuccessStore } from '../store/success'
+import { useErrorStore } from '../store/error'
 
 Vue.use(Router)
 Vue.use(Meta)
@@ -17,15 +20,22 @@ const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  const successStore = useSuccessStore()
+  const errorStore = useErrorStore()
+
   checkForUpdates()
   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
-  const isTokenSet = store.getters.isTokenSet
+  // const isTokenSet = store.getters.isTokenSet
+  const isTokenSet = authStore.isTokenSet
   if (requiresAuth && !isTokenSet) {
     return next('/login')
   }
   checkIfTokenNeedsRefresh()
-  store.commit(types.SUCCESS, null)
-  store.commit(types.ERROR, null)
+  // store.commit(types.SUCCESS, null)
+  successStore.success(null)
+  // store.commit(types.ERROR, null)
+  errorStore.error(null)
   return next()
 })
 
